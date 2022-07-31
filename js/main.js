7b90523559b58a4bfc9cde6c00f5ed101c3a54e3
sayfutdiinov <sayfutdinovsaman@gmail.com>
2022-07-31 13:31:31 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var elBurrgerBtn=document.querySelector(".toggle-btn");
+var elHrNavmb=document.querySelector(".header-site");
+
+
+
+elBurrgerBtn.addEventListener('click', function(){
+    elHrNavmb.classList.toggle("header-naw-open")
+})
